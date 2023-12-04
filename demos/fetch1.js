af45ddde2b104b8eef615da62bd61bca07537385
Craig McKeachie <craigdmckeachie@gmail.com>
2023-12-04 15:57:25 -0500

    first fetch example

---
+++ demos/fetch1.js
@@ -0,0 +1,3 @@
+fetch("http://jsonplaceholder.typicode.com/users/1")
+  .then((response) => response.json()) //taking the body of the response and converting the string json data to a javascript object
+  .then((data) => console.log(data.name)); //receives the javascript object so you can access any of the properties
