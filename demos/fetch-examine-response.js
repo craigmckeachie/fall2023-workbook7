63ad784408ca020d6528d89e4bfbb39aab2e64ea
Craig McKeachie <craigdmckeachie@gmail.com>
2023-12-06 08:56:13 -0500

    fix examine response

--- demos/fetch-examine-response.js
+++ demos/fetch-examine-response.js
@@ -1,15 +1,8 @@
-//many, array
 fetch("https://jsonplaceholder.typicode.com/albums")
-  .then((response) => response.json())
+  .then((response) => {
+    console.log(response.status, response.statusText);
+    return response.json();
+  })
   .then((albums) => {
-    for (const album of albums) {
-      console.log(album.title);
-    }
-  });
-
-//one, object
-fetch("https://jsonplaceholder.typicode.com/albums/1")
-  .then((response) => response.json())
-  .then((album) => {
-    console.log(album.title);
+    console.log(albums);
   });
