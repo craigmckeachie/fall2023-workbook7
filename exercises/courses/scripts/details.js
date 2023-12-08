fa05b957815fdca8158c2bb41928b275bb04bd3c
Craig McKeachie <craigdmckeachie@gmail.com>
2023-12-08 14:18:53 -0500

    update

---
+++ exercises/courses/scripts/details.js
@@ -0,0 +1,20 @@
+// get html elements
+//p1
+//p2
+//p3
+
+
+//write functions
+function displayCourse(course) {
+    //p1 innerText course.something
+
+}
+
+function initialize() {
+    //fetch a course from api
+    //pass the course to displayCourse
+}
+
+
+//wire-up functions to html element events
+window.onload = initialize;
