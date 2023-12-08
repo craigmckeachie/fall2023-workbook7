be93622cbb06ee9ec48b827ac9d2e044f118fddb
Craig McKeachie <craigdmckeachie@gmail.com>
2023-12-08 10:24:20 -0500

    adding comments

--- exercises/courses/scripts/courses.js
+++ exercises/courses/scripts/courses.js
@@ -1,25 +1,36 @@
 "use strict";
 
 //get the HTML elements
+//get table body so we can insert rows for each course
 const coursesTableBody = document.getElementById("coursesTableBody");
 
-//write function
+//takes a courses array
+//loops through the array to get each course (one at a time)
+//creates a table row and cells and fills in the course data inside the cells
 function loadTableBody(courses) {
+  //loops through the array to get each course (one at a time)
   for (const course of courses) {
-    console.log(course.courseName);
+    //creates a table row (tr) for each course and appends or adds the row inside the table body
     let row = coursesTableBody.insertRow();
 
+    //inside the row create a table cell (td)
+    //put the course department name inside the table cell
     let cell1 = row.insertCell();
     cell1.innerText = course.dept;
 
+    //inside the row create a table cell (td)
+    //put the course number inside the table cell
     let cell2 = row.insertCell();
     cell2.innerText = course.courseNum;
 
+    //inside the row create a table cell (td)
+    //put the course name inside the table cell
     let cell3 = row.insertCell();
     cell3.innerText = course.courseName;
   }
 }
 
+//what to do initially after the web page loads all the html
 function initialize() {
   fetch("http://localhost:8081/api/courses")
     .then((response) => response.json())
@@ -27,4 +38,11 @@ function initialize() {
       loadTableBody(courses);
     });
 }
+
+//wiring up or telling the web page to call the initialize function after the html has been loaded
 window.onload = initialize;
+
+
+
+ 
+
